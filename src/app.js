252086678c27a9285461ff550db7681ef9934628
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-04 02:39:58 -0300

    Inclui rotas da aplicacao

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { errors } = require('celebrate');
+const apiRouter = require('./routes');
 
 const app = express();
-const apiRouter = express.Router();
 
 app.use('/api', apiRouter);
 app.use(cors({ credentials: true }));
-app.use(errors());
 
 module.exports = app;
