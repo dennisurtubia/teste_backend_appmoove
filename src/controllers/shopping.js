9104564ba4856a19738fc14d19cba6088219955e
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-04 11:51:46 -0300

    Atualiza controller adicionando verificacoes

--- src/controllers/shopping.js
+++ src/controllers/shopping.js
@@ -1,18 +1,19 @@
 /* eslint-disable camelcase */
 const knex = require('../../db/knex');
+const { cards } = require('../utils/cards');
+
+async function rejectTransaction(produto_id, qtde_comprada, valor_unitario) {
+  await knex('transacoes').insert({
+    produto_id,
+    valor_venda: qtde_comprada * valor_unitario,
+    data_venda: knex.fn.now(),
+    estado: 'REJEITADO',
+  });
+}
 
 module.exports = {
   async sale(req, res) {
     try {
-      const cards = {
-        Visa: /^4[0-9]{12}(?:[0-9]{3})/,
-        Mastercard: /^5[1-5][0-9]{14}/,
-        Amex: /^3[47][0-9]{13}/,
-        DinersClub: /^3(?:0[0-5]|[68][0-9])[0-9]{11}/,
-        Discover: /^6(?:011|5[0-9]{2})[0-9]{12}/,
-        JCB: /^(?:2131|1800|35\d{3})\d{11}/
-      };
-
       const { produto_id, qtde_comprada, cartao } = req.body;
 
       const product = await knex
@@ -21,16 +22,14 @@ module.exports = {
         .where('id', produto_id)
         .first();
 
-      if (product.qtde_estoque === 0) throw new Error('PRODUCT_NOT_IN_STOCK');
-
       if (!cartao.numero.match(cards[cartao.bandeira])) {
-        await knex('transacoes').insert({
-          produto_id,
-          valor_venda: qtde_comprada * product.valor_unitario,
-          data_venda: knex.fn.now(),
-          estado: 'REJEITADO',
-        });
+        rejectTransaction(produto_id, qtde_comprada, product.valor_unitario);
         throw new Error('INVALID_CARD');
+      }
+
+      if (product.qtde_estoque < qtde_comprada) {
+        rejectTransaction(produto_id, qtde_comprada, product.valor_unitario);
+        throw new Error('PRODUCT_NOT_IN_STOCK');
       }
 
       await knex('transacoes').insert({
@@ -48,7 +47,7 @@ module.exports = {
         message: 'Venda realizada com sucesso',
       });
     } catch (error) {
-      if (error.message === 'INVALID_CARD') {
+      if (error.message === 'INVALID_CARD' || error.message === 'PRODUCT_NOT_IN_STOCK') {
         res.status(412).json({
           message: 'Os valores informados não são válidos.',
         });
